db5dd35e1652e334067aa53d7777771d97e627f3
NadiaPia <206i417@gmail.com>
2022-10-27 05:01:52 +0000

    total number of  products and categories are display on the dashbord

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   
   def show
+    @count_products = Product.count
+    @count_categories = Category.count
+
   end
+
+  
 end
